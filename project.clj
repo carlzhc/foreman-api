73f1ff0848315f801fd4fbc78c8d39a4602a9cba
Carl Zhang <59pyl@ms.com>
2019-05-17 13:07:29 +0800

    Add architectures

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject foreman-api "0.1.0-SNAPSHOT"
+  :description "Clojure lib for foreman api 2"
+  :url "http://github.com/carlzhc/foreman-api"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [http-kit "2.3.0"]
+                 [environ "1.1.0"]]
+  :repl-options {:init-ns foreman-api.resource})
---
+++ src/foreman_api/connection.clj
@@ -0,0 +1,24 @@
+(ns foreman-api.connection
+  (:require [clojure.string :as str]
+           [org.httpkit.client :as http]
+           [environ.core :refer [env]]))
+
+(def ^:dynamic *host*
+  (atom
+   (or (env :foreman-host) "https://localhost")))
+
+(def ^:dynamic *connection*
+  (atom
+   {:headers {"content-type" "application/json; charset=utf-8"}
+    :timeout  (if-let [v (env :foreman-timeout)]
+                (Integer. v)
+                3000)
+    :keepalive (if-let [v (env :foreman-keepalive)]
+                (Integer. v)
+                3000)
+    :insecure? (if-let [v (env :foreman-insecure)]
+                 ({"true" true "false" false
+                   "yes" true "no" false} v)
+                 true)
+    :basic-auth (when-let [v (env :foreman-auth)]
+                  (take 2 (str/split v #":")))}))
